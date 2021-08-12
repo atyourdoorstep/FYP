0fc981668844b07dcb6d0dd6c4f49479da00b3b2
atyourdoorstep <atyourdoorstep.pk@gmail.com>
2021-08-12 15:54:03 +0500

    profile+user+queryOptimisation(for update and privileges table)+prifile pic done image error removed

--- config/database.php
+++ config/database.php
@@ -58,42 +58,42 @@ return [
 //            'engine' => null,
 //        ],
 
-//inf
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'mysql-43979-0.cloudclusters.net'),
-            'port' => env('DB_PORT', '19982'),
-            'database' => env('DB_DATABASE', 'atyourdoorstep'),
-            'username' => env('DB_USERNAME', 'admin'),
-            'password' => env('DB_PASSWORD', 'EEQjyiQv'),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-        ],
-
-        //default
+//sqlCluster
 //        'mysql' => [
 //            'driver' => 'mysql',
 //            'url' => env('DATABASE_URL'),
-//            'host' => env('DB_HOST', '127.0.0.1'),
-//            'port' => env('DB_PORT', '3306'),
-//            'database' => env('DB_DATABASE', 'forge'),
-//            'username' => env('DB_USERNAME', 'forge'),
-//            'password' => env('DB_PASSWORD', ''),
+//            'host' => env('DB_HOST', 'mysql-43979-0.cloudclusters.net'),
+//            'port' => env('DB_PORT', '19982'),
+//            'database' => env('DB_DATABASE', 'atyourdoorstep'),
+//            'username' => env('DB_USERNAME', 'admin'),
+//            'password' => env('DB_PASSWORD', 'EEQjyiQv'),
 //            'unix_socket' => env('DB_SOCKET', ''),
 //            'charset' => 'utf8mb4',
 //            'collation' => 'utf8mb4_unicode_ci',
 //            'prefix' => '',
-//            'prefix_indexes' => true,
 //            'strict' => true,
-//            'engine' => null,
-//            'options' => extension_loaded('pdo_mysql') ? array_filter([
-//                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-//            ]) : [],
 //        ],
+
+        //default
+        'mysql' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
