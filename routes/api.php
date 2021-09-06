7ed6283918b294755c59df79d6ee5a113a415a4d
atyourdoorstep <atyourdoorstep.pk@gmail.com>
2021-09-06 21:48:42 +0500

    jwt middleware

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+Route::post('/jwtmiddelwarecheck'
+    ,
+    function (Request $request)
+    {
+       return $request->all();
+    }
+)->middleware('JwtAuthUser');
 
 
 
