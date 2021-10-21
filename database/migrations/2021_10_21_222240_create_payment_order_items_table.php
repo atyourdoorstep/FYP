32ed9f8e1e720bbf83d9e439d50cec66bc28465c
atyourdoorstep <atyourdoorstep.pk@gmail.com>
2021-10-22 03:38:32 +0500

    working on payment types and payment status against order_items

---
+++ database/migrations/2021_10_21_222240_create_payment_order_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_order_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('stripe_payment_id')->nullable();
+            $table->string('type')->default('cash on delivery');
+            $table->string('status')->default('pending');
+            $table->unsignedBigInteger('order_item_id');
+            $table->index('order_item_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_order_items');
+    }
+}
