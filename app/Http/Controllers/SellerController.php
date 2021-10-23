ba287e4fb3008d42258fcc223d40ea3fee88c301
atyourdoorstep <atyourdoorstep.pk@gmail.com>
2021-10-23 17:48:24 +0500

    working on payment types and payment status against order_items

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -6,6 +6,8 @@ use App\Models\Seller;
 use App\Models\SellerFolder;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class SellerController extends Controller
@@ -41,6 +43,27 @@ class SellerController extends Controller
                 'seller'=>$seller,
             ]
             ,200
+        );
+    }
+    public function sellerShowProfile(Request $request)
+    {
+        $seller = \App\Models\Seller::find($request->all()['id']);
+        $sellerItems=$seller->items;
+        $thCat=Arr::pluck(DB::table('categories')
+            ->select('id')
+            ->whereIn('id', Arr::pluck($sellerItems, 'category_id'))
+            ->get(), 'id');
+
+//    $a = \App\Models\Item::with('category.category')->where('seller_id', $user->seller->id)->get();
+        $catItem = \App\Models\Category::with([
+            'items'=>fn($query)=>$query->whereIn('id',Arr::pluck($sellerItems, 'id'))
+        ])->whereIn('id',$thCat)->get();
+        return response()->json(
+            [
+                'success' => true,
+                'profile' => $seller->user->profile,
+                'catItems'=>$catItem,
+            ], 200
         );
     }
     public function getSellerInfo(Request $request)
